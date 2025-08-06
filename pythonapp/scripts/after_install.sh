71ab450dce3c6b5c9322d1f9b6b3692cc4fd9d64
systemi <systemi@systeminoMBP.lan>
2025-08-06 17:11:19 +0900

    fix after_install.sh

--- pythonapp/scripts/after_install.sh
+++ pythonapp/scripts/after_install.sh
@@ -5,7 +5,7 @@ cd /opt/aws-vuessg-fastapi-sample/pythonapp
 
 # Install dependencies globally for the python3 interpreter using uv
 # No virtual environment is used.
-/root/.local/bin/uv sync
+/home/ec2-user/.local/bin/uv sync
 
 # Create a systemd service file for the FastAPI application
 cat > /etc/systemd/system/fastapi-app.service << EOL
@@ -14,14 +14,15 @@ Description=FastAPI application
 After=network.target
 
 [Service]
-User=root
-Group=root
+User=ec2-user
+Group=ec2-user
 WorkingDirectory=/opt/aws-vuessg-fastapi-sample/pythonapp
-# 環境変数を利用する場合は、以下をコメントインすること
+EnvironmentFile=/home/ec2-user/.local/bin/env
+# 環境変数を利用する場合は、以下をコメントインすること。.envファイルの作成が必要です
 # EnvironmentFile=/opt/aws-vuessg-fastapi-sample/pythonapp/.env
 
 # The uvicorn executable is installed in /usr/local/bin by uv
-ExecStart=/root/.local/bin/uv run uvicorn app:create_app --host 0.0.0.0 --port 8000 --factory --timeout-keep-alive 300 --workers 2
+ExecStart=/home/ec2-user/.local/bin/uv run uvicorn app:create_app --host 0.0.0.0 --port 8000 --factory --timeout-keep-alive 300 --workers 2
 Restart=always
 
 [Install]
