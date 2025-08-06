3395f5155a6f528353dcc92db350be0178bddc49
systemi <systemi@systeminoMBP.lan>
2025-08-07 00:01:23 +0900

    fix artifact bucket

--- pythonapp/deploy.sh
+++ pythonapp/deploy.sh
@@ -10,7 +10,7 @@ else
 fi
 
 # Check if all required variables are set
-required_vars=("AWS_REGION" "STACK_NAME" "CODE_DEPLOY_APP_NAME" "CODE_DEPLOY_GROUP_NAME" "GITHUB_OWNER" "GITHUB_REPO" "GITHUB_BRANCH" "SSM_PARAM_NAME_FOR_CONNECTION_ARN")
+required_vars=("AWS_REGION" "STACK_NAME" "CODE_DEPLOY_APP_NAME" "CODE_DEPLOY_GROUP_NAME" "GITHUB_OWNER" "GITHUB_REPO" "GITHUB_BRANCH" "SSM_PARAM_NAME_FOR_CONNECTION_ARN" "SSM_PARAM_NAME_FOR_PIPELINE_ARTIFACT_BUCKET")
 for var in "${required_vars[@]}"; do
     if [ -z "${!var}" ]; then
         echo "Error: Environment variable $var is not set in deploy.env" >&2
@@ -31,9 +31,7 @@ aws cloudformation deploy \
     GitHubBranch="$GITHUB_BRANCH" \
     GitHubConnectionArnParameterName="$SSM_PARAM_NAME_FOR_CONNECTION_ARN" \
     CodeDeployApplicationName="$CODE_DEPLOY_APP_NAME" \
-    CodeDeployDeploymentGroup="$CODE_DEPLOY_GROUP_NAME" \
-  --capabilities CAPABILITY_IAM \
-  --no-fail-on-empty-changeset
+    CodeDeployDeploymentGroup="$CODE_DEPLOY_GROUP_NAME"     PipelineArtifactBucketParameterName="$SSM_PARAM_NAME_FOR_PIPELINE_ARTIFACT_BUCKET"   --capabilities CAPABILITY_IAM   --no-fail-on-empty-changeset
 
 echo "CloudFormation deployment command executed successfully."
 echo "Check the AWS CloudFormation console for the status of the stack: $STACK_NAME"
