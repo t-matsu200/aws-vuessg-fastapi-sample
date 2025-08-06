8ce35cd005a56b40f6567a3141ebd178cd3c6c35
systemi <systemi@systeminoMBP.lan>
2025-08-06 17:11:29 +0900

    fix before_install.sh

--- pythonapp/scripts/before_install.sh
+++ pythonapp/scripts/before_install.sh
@@ -11,22 +11,30 @@ rm -rf /opt/aws-vuessg-fastapi-sample/pythonapp_old
 if [ -d "/opt/aws-vuessg-fastapi-sample/pythonapp" ]; then
     mv /opt/aws-vuessg-fastapi-sample/pythonapp /opt/aws-vuessg-fastapi-sample/pythonapp_old
 fi
+chown -R ec2-user /opt/aws-vuessg-fastapi-sample/pythonapp
 
 yum remove -y python3
 
+UV_PATH="/root/.local/bin/uv"
+
 # Install uv if not present
-if ! command -v uv &> /dev/null
+if ! command -v $UV_PATH &> /dev/null
 then
     echo "uv could not be found, installing..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
+    // ec2-user が uv を実行できるようにするため、ec2-user のホームディレクトリにも uv をインストールする
+    su ec2-user
+    curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
+
+su root
 
 # Install Python 3.13 if not already present
 PYTHON_VERSION="3.13.5"
 # Check if python3 command points to the correct version
 if ! (command -v python3 && python3 --version | grep -q "Python $PYTHON_VERSION"); then
     echo "Python 3.13 not found or not linked, installing/linking..."
-    /root/.local/bin/uv python install $PYTHON_VERSION --install-dir /opt/uv-python/
+    $UV_PATH python install $PYTHON_VERSION --install-dir /opt/uv-python/
     chmod +x /opt/uv-python/cpython-$PYTHON_VERSION-linux-x86_64-gnu/bin/python3
     # Create a symbolic link to make python3.13 the default python3
     rm -f /usr/bin/python3 /usr/local/bin/python3
