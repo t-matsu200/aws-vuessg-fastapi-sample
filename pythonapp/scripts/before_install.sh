0281abb49de55fbfbaac205475c6a436c20c6a53
systemi <systemi@systeminoMBP.lan>
2025-08-06 16:38:53 +0900

    fix before_install.sh

--- pythonapp/scripts/before_install.sh
+++ pythonapp/scripts/before_install.sh
@@ -12,34 +12,24 @@ if [ -d "/opt/aws-vuessg-fastapi-sample/pythonapp" ]; then
     mv /opt/aws-vuessg-fastapi-sample/pythonapp /opt/aws-vuessg-fastapi-sample/pythonapp_old
 fi
 
-# Install build dependencies for Python (for Amazon Linux 2)
-yum groupinstall -y "Development Tools"
-yum install -y bzip2 bzip2-devel gcc git libffi-devel readline readline-devel sqlite sqlite-devel zlib-devel libdb-devel gdbm-devel xz-devel tk-devel uuid-devel libuuid-devel
-yum install -y openssl-devel
 yum remove -y python3
-
-# Download, compile, and install Python 3.13 if not already present
-PYTHON_VERSION="3.13.3"
-PYTHON_VERSION_SHORT="3.13"
-# Check if python3 command points to the correct version
-if ! (command -v python3 && python3 --version | grep -q "Python $PYTHON_VERSION"); then
-    echo "Python 3.13 not found or not linked, installing/linking..."
-    cd /tmp
-    curl -O https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
-    tar -xzf Python-$PYTHON_VERSION.tgz
-    cd Python-$PYTHON_VERSION
-    ./configure --with-ssl
-    make install
-    # Create a symbolic link to make python3.13 the default python3
-    ln -sf /usr/local/bin/python$PYTHON_VERSION_SHORT /usr/local/bin/python3
-    # Clean up source files
-    cd /
-    rm -rf /tmp/Python-$PYTHON_VERSION*
-fi
 
 # Install uv if not present
 if ! command -v uv &> /dev/null
 then
     echo "uv could not be found, installing..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
+
+# Install Python 3.13 if not already present
+PYTHON_VERSION="3.13.5"
+# Check if python3 command points to the correct version
+if ! (command -v python3 && python3 --version | grep -q "Python $PYTHON_VERSION"); then
+    echo "Python 3.13 not found or not linked, installing/linking..."
+    /root/.local/bin/uv python install $PYTHON_VERSION --install-dir /opt/uv-python/
+    chmod +x /opt/uv-python/cpython-$PYTHON_VERSION-linux-x86_64-gnu/bin/python3
+    # Create a symbolic link to make python3.13 the default python3
+    rm /usr/bin/python3 /usr/local/bin/python3
+    ln -s /opt/uv-python/cpython-$PYTHON_VERSION-linux-x86_64-gnu/bin/python3 /usr/bin/python3
+    ln -s /opt/uv-python/cpython-$PYTHON_VERSION-linux-x86_64-gnu/bin/python3 /usr/local/bin/python3
 fi
