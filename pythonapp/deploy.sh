4a81d5c0d1d5935311de25157e628b1cbb12424e
systemi <systemi@systeminoMBP.lan>
2025-08-07 00:32:25 +0900

    fix deploy.sh

--- pythonapp/deploy.sh
+++ pythonapp/deploy.sh
@@ -31,7 +31,10 @@ aws cloudformation deploy \
     GitHubBranch="$GITHUB_BRANCH" \
     GitHubConnectionArnParameterName="$SSM_PARAM_NAME_FOR_CONNECTION_ARN" \
     CodeDeployApplicationName="$CODE_DEPLOY_APP_NAME" \
-    CodeDeployDeploymentGroup="$CODE_DEPLOY_GROUP_NAME"     PipelineArtifactBucketParameterName="$SSM_PARAM_NAME_FOR_PIPELINE_ARTIFACT_BUCKET"   --capabilities CAPABILITY_IAM   --no-fail-on-empty-changeset
+    CodeDeployDeploymentGroup="$CODE_DEPLOY_GROUP_NAME" \
+    PipelineArtifactBucketParameterName="$SSM_PARAM_NAME_FOR_PIPELINE_ARTIFACT_BUCKET" \
+  --capabilities CAPABILITY_IAM \
+  --no-fail-on-empty-changeset
 
 echo "CloudFormation deployment command executed successfully."
 echo "Check the AWS CloudFormation console for the status of the stack: $STACK_NAME"
